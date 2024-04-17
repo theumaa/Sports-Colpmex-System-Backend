98ccea594c6fe4adcf6f3bb41e8a16e69f5f62a8
Tharindu Umayanga <tharinduumayangat@gmail.com>
2024-04-17 19:49:19 +0530

    add migration file

---
+++ database/migrations/2024_11_26_161837_create_user_trackings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_trackings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('hours_left');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_trackings');
+    }
+};
